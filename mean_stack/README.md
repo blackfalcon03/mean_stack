5271fc3f290ff3d39cd180ed958cc2f15ae79dd0
Ubuntu <ubuntu@ip-172-31-23-105.ap-south-1.compute.internal>
2017-02-18 19:06:49 +0000

    sycn all changes

---
+++ mean_stack/README.md
@@ -0,0 +1,2 @@
+# mean_stack
+Tutorial code to understand MEAN stack from http://adrianmejia.com/blog/2014/10/03/mean-stack-tutorial-mongodb-expressjs-angularjs-nodejs/
---
+++ mean_stack/mean_stack/todo-app/todo-api/models/Todo.js
@@ -0,0 +1,12 @@
+//Add library for Mongoose
+var mongoose = require('mongoose');
+
+var TodoSchema=new mongoose.Schema({
+	name: String,
+	completed: Boolean,
+	note: String,
+	updated_at: {type:Date, default: Date.now },
+});
+
+module.exports = mongoose.model('TodoModel',TodoSchema);
+
